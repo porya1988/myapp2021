07b931d3b1e9ee332d9022c9125038b9d1f80baa
porya1988 <porya.sahraee@gmail.com>
2021-08-27 18:38:44 +0430

    fourth

--- app/src/main/java/com/example/myapp2021/foodDetail/FoodDetailActivity.java
+++ app/src/main/java/com/example/myapp2021/foodDetail/FoodDetailActivity.java
@@ -1,15 +1,13 @@
 package com.example.myapp2021.foodDetail;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.example.myapp2021.R;
 import com.example.myapp2021.config.AppConfiguration;
 import com.example.myapp2021.databinding.ActivityFooddetailBinding;
 import com.example.myapp2021.model.MFoods;
