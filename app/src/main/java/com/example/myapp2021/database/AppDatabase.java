c35eac57fbc07c9b538896cd5cd1156ed690143d
porya1988 <porya.sahraee@gmail.com>
2021-08-28 18:09:44 +0430

    fifth commit

--- app/src/main/java/com/example/myapp2021/database/AppDatabase.java
+++ app/src/main/java/com/example/myapp2021/database/AppDatabase.java
@@ -1,4 +1,28 @@
 package com.example.myapp2021.database;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.myapp2021.config.AppConfiguration;
+import com.example.myapp2021.model.MFoods;
+
+@Database(entities = {MFoods.class},version = 1,exportSchema = true)
+public  abstract class AppDatabase extends RoomDatabase {
+
+    public abstract IDao iDao();
+    private static AppDatabase instance=null;
+
+    public static synchronized AppDatabase getInstance(Context context){
+
+        if(instance==null){
+            instance= Room.databaseBuilder(context,AppDatabase.class,"tbl_foods").
+                    allowMainThreadQueries().build();
+
+        }
+        return instance;
+
+    }
 }
